b03dea899499a4ebc0371bbf8d694c02b939094a
Mls <ersoykartal@outlook.com>
2026-09-14 02:34:56 +0300

    Exclude kernel from recovery image properly

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,9 +43,10 @@ BOARD_KERNEL_IMAGE_NAME := Image
 # TARGET_KERNEL_SOURCE := kernel/xiaomi/aurora
 
 # Kernel - prebuilt
-# TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-# TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 endif
 
 # Partitions
